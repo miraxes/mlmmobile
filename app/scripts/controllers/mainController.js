c3a0083aebc53d1ef4cf84285e2f2acec3c2ccec
miraxes <mr.miraxes@gmail.com>
2016-08-09 11:22:48 +0300

    init

---
+++ app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name MlmMobile.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('MlmMobile')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name MlmMobile.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('MlmMobile')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
